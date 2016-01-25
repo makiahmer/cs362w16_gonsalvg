a621d2bd1f2423200b1746a44a3e21056788338a
gonsalvg <gonsalvg@oregonstate.edu>
2016-01-24 20:10:20 -0800

    fix attempt

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -643,7 +643,8 @@ int getCost(int cardNumber)
   return -1;
 }
 
-int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
+int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
+*state, int handPos, int *bonus)
 {
   int i;
   int j;
--- dominion/dominion.h
+++ dominion/dominion.h
@@ -78,7 +78,7 @@ struct gameState {
 
 struct gameState* newGame();
 
-int adventurer_cardEffect(struct gameState *state, int drawntreasure, int cardDrawn, int* temphand, int z){
+int adventurer_cardEffect(struct gameState **state, int drawntreasure, int cardDrawn, int* temphand, int z){
   while(drawntreasure<2)
   {
      if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
@@ -97,7 +97,7 @@ int adventurer_cardEffect(struct gameState *state, int drawntreasure, int cardDr
          z++;
       }
    }
-   
+
    while(z-1>=0){
       state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
       z=z-1;

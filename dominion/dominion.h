de2e3ee6b2c2feb76cf07d9e3188c65ad42531ac
gonsalvg <gonsalvg@oregonstate.edu>
2016-01-24 20:35:36 -0800

    jk another fix

--- dominion/dominion.h
+++ dominion/dominion.h
@@ -78,33 +78,8 @@ struct gameState {
 
 struct gameState* newGame();
 
-int adventurer_cardEffect(struct gameState *state, int drawntreasure, int cardDrawn, int *temphand, int currentPlayer, int z){
-  while(drawntreasure < 2)
-  {
-     if (state->deckCount[currentPlayer] < 1){//if the deck is empty we need to shuffle discard and add to deck
-        shuffle(currentPlayer, state);
-     }
+int adventurer_cardEffect(struct gameState *state, int drawntreasure, int cardDrawn, int *temphand, int currentPlayer, int z);
 
-     drawCard(currentPlayer, state);
-     cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-
-     if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold) drawntreasure++;
-
-     else
-     {
-         temphand[z]=cardDrawn;
-         state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-         z++;
-      }
-   }
-
-   while(z-1>=0){
-      state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-      z=z-1;
-  }
-
-  return 0;
-}
 int* kingdomCards(int k1, int k2, int k3, int k4, int k5, int k6, int k7,
 		  int k8, int k9, int k10);
 

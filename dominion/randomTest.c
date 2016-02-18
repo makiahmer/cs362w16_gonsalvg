52a19f2edbc969190527ab1af52bfafa7ce656f1
gonsalvg <gonsalvg@oregonstate.edu>
2016-02-17 19:24:53 -0800

    Assignment 3

--- dominion/randomTest.c
+++ dominion/randomTest.c
@@ -10,12 +10,55 @@
 #include <stdlib.h>
 #include <assert.h>
 
-#define NUM_TESTS 1
+#define MAX_TESTS 1200
 
-int main(){
-   int deckArr[10] = {adventurer, gardens, embargo, village, minion, mine,
-       cutpurse, sea_hag, tribute, smithy};
+//This randomly tests smithy
+
+int main() {
+
+	  int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, 
+	       sea_hag, tribute, smithy};
+
+	  int i, j, n, players, player, handCount, deckCount, seed, address;
+	  //struct gameState state;
+	  struct gameState state;
+	  struct gameState stat;
+	  struct gameState sta;
+
+	  printf("Running Random Card Test for Smithy\n");
+
+	  /*
+										--- Author's Note ---
+	  So, I had problems running out of memory when I used the same gameState variable more than 12 times, and
+	  I honestly don't know why. I momentarily solved this problem by adding more for loops and creating more gamestates;
+	  I was still able to get decent coverage, though not up to the amount of tests I originally had in mind.
+	  (I just put this on the second file as well)
+
+	  This program wouldn't work without the printouts, oddly enough.
+	  */
+
+	  for (i = 0; i < MAX_TESTS; i++) {
+
+		  
+		 players = rand() % 4;
+		 seed = rand();		//pick random seed
+		
+		 initializeGame(players, k, seed, &state);	//initialize Gamestate
+
+		  //Initiate valid state variables
+		  state.deckCount[player] = rand() % MAX_DECK; //Pick random deck size out of MAX DECK size
+		  state.discardCount[player] = rand() % MAX_DECK;
+		  state.handCount[player] = rand() % MAX_HAND;
 
 
-   return 0;
-}
+		  //Copy state variables
+		  handCount = state.handCount[player];
+		  deckCount = state.deckCount[player];
+
+		  		  	  		  		  printf("%d\n", i);
+
+
+		  cardEffect(smithy, 1, 1, 1, &state);		//Run adventurer card
+
+		  printf("%dB\n", i);
+	  }
--- dominion/randomTestAdventurer_gonsalvg.c
+++ dominion/randomTestAdventurer_gonsalvg.c
@@ -14,24 +14,48 @@
 
 void randomHands(int deckArr, int deckCount, int handCount);
 
-/*test will generate many different hands, then use the card and test some of
-the results*/
+/*tests the effect of the adventured with randomized gamestates */
 int main(){
    int deckArr[10] = {adventurer, gardens, embargo, village, minion, mine,
        cutpurse, sea_hag, tribute, smithy};
-   int seed, players, handCount, deckCount, address;
-
+   
+	int seed, players, player, beforeCount, card1, card2;
+   
+	struct gameState state;
+   
+	printf("Running randomized adventurer card test.\n");
+	
    for (int i = 0; i < NUM_TESTS; i++){
       seed = rand();
       players = rand() % 4;
-
-
+		player = rand() % players; //choose a random player out of the ones we created.
+		//player = 1;
+		initializeGame(players, deckArr, seed, &state);
+      
+		state.deckCount[player] = rand() % MAX_DECK; //Pick random deck size out of MAX DECK size
+		state.discardCount[player] = rand() % MAX_DECK;
+		state.handCount[player] = rand() % MAX_HAND;
+		
+		if (seed % 3 == 0) state.deckCount[player] = 0;
+		
+		beforeCount = state.handCount[player]; //get the count of the player's hand before adventurer card played
+		
+		//1 in 3 chance of making an empty deck.
+		
+		
+		cardEffect(adventurer, 1, 1, 1, &state); //run the card
+		
+		beforeCount += 2; // current count should equal before +2.
+		
+		card1 = state.hand[player][state.handCount[player] - 1];
+		card2 = state.hand[player][state.handCount[player] - 2];
+		
+		if (state.handCount[player] != beforeCount) printf("Error: player did not draw two cards.\n");
+		
+		if (card1 != copper || card1 != silver || card1 != gold) printf("Error: player didn't recieve 1st treasure.\n");
+		if (card2 != copper || card2 != silver || card2 != gold) printf("Error: player didn't recieve 2nd treasure.\n"); 
    }
+   
 
    return 0;
-}
-
-void randomHands(int deckArr, int deckCount, int handCount){
-
-
 }
